14f1263715dba86937185ebeda9d9a42b8bedc05
reggyshicky <reginahshikanda@gmail.com>
2023-04-18 02:08:48 -0700

    finding the command inputs from the user

--- find_cmdinputs.c
+++ find_cmdinputs.c
@@ -9,7 +9,7 @@
 int find_cmdinputs(char *element, char **args)
 {
 	char *sub_tokens; /*stores subsequent tokens*/
-	char *_track; /*tracking our pos in the input str*/
+	char *_track = element; /*tracking our pos in the input str*/
 	char *cmd; /*stores the 1st token, args[0]*/
 
 	int l = 0;
@@ -24,6 +24,6 @@ int find_cmdinputs(char *element, char **args)
 		_track = sub_tokens; /*checks for more tokens in the next loop*/
 		args[l] = sub_tokens;
 	}
-	args[l] = NULL;
+	args[l] = NULL; /*shows end of the array*/
 	return (l);
 }
