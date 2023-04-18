3a66b7cc137ae6bb20877f775abe354b757094c8
reggyshicky <reginahshikanda@gmail.com>
2023-04-18 02:03:19 -0700

    finding the command inputs from the user

---
+++ find_cmdinputs.c
@@ -0,0 +1,29 @@
+#include "simple_shell.h"
+/**
+ * find_cmdinputs - Displays inputs from the user in an array
+ * @element: The string input by the user
+ * @args: Stores the args extracted by the function
+ * Return: length of args(array)
+ */
+
+int find_cmdinputs(char *element, char **args)
+{
+	char *sub_tokens; /*stores subsequent tokens*/
+	char *_track; /*tracking our pos in the input str*/
+	char *cmd; /*stores the 1st token, args[0]*/
+
+	int l = 0;
+
+	cmd = strtok(element, "\n\t\r");
+	args[l] = cmd;
+
+	while (_track != NULL)
+	{
+		++l;
+		sub_tokens = strtok(NULL, "\n\t\r");
+		_track = sub_tokens; /*checks for more tokens in the next loop*/
+		args[l] = sub_tokens;
+	}
+	args[l] = NULL;
+	return (l);
+}
